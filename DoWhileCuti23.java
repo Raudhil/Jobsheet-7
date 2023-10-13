3202947eaf4057fcf5582323d394a50530db8494
Raudhil <raudhilfirdausn@gmail.com>
2023-10-13 07:19:10 +0700

    DoWhileCuti23

--- DoWhileCuti23.java
+++ DoWhileCuti23.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class DoWhileCuti23 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int jatahCuti, jumlahHari;
